fef7fc5ae8d8de311435d9b4f05ffd6f5e4d1b65
julian windeck <warumdrum1@gmail.com>
2021-07-16 20:56:11 +0100

    Add aging Access Points
    Till now, wsniff just collected more and more APs,
    meaning the table of detected APs continuously grew
    when moving in 'Detect APs'-mode. Now access points
    'age' in this mode, so they are now no longer shown
    when you leave their reach.

--- website/aps.py
+++ website/aps.py
@@ -1,21 +1,66 @@
 from threading import Thread, Event, Lock
-from time import sleep
-import os
+from time import sleep, time
+import random, os
 
 from scapy.all import *
 
-aps = dict() #all APs that are uncovered
 
-#used to synchronize
-_running = Event()
-#lock is not really needed here because there is just one thread writing and one reading
-lock = Lock()
+class AccessPoint():
+
+    def __init__(self, bssid, ssid, channel):
+        self.bssid = bssid
+        self.ssid = ssid
+        self.channel = channel
+
+        self.t_last_seen = time()
+    
+    def isAlive(self, t_death=20):
+        return time() - self.t_last_seen < t_death
+
+    def refresh(self):
+        self.t_last_seen = time
+
+    def __str__(self):
+        return f"[{self.bssid}] {self.ssid} on channel {self.channel}"
+
+
+#bssid as unique identifier, AccessPoint object as value
+lock = Lock() #lock is not really needed here because there is just one thread writing and one reading
+aps = dict() #all APs that are uncovered
+_running = Event() #used to synchronize
+
+
+def print_access_points():
+    for ap in aps.values():
+        print(ap)
+
+def clean(t_remove, t_sleep=30):
+    while _running.is_set():
+        sleep(t_sleep)
+        
+        #remove all dead access points
+        bssids = tuple(aps.keys())
+        for bssid in bssids:
+            if not aps[bssid].isAlive(t_remove):
+                del aps[bssid]
+        
+
+def found_access_point(bssid, ssid, channel):
+    #found new access point: add it to dict
+    if bssid not in aps:
+        ap = AccessPoint(bssid, ssid, channel)
+        aps[bssid] = ap
+    else:
+        #acess point already in dict -> refresh AP to prevent it from getting deleted
+        aps[bssid].refresh()
+
+
 
 def hopper(iface):
     channel = 1
     stop_hopper = False
     while _running.is_set():
-        time.sleep(0.25)
+        sleep(0.25)
         os.system(f"sudo iwconfig {iface} channel {channel}")
         #print(f"[*] current channel {channel}")
         
@@ -33,9 +78,7 @@ def handlePacket(pkt):
             ssid = pkt.getlayer(Dot11Elt).info.decode("utf-8")
             channel = pkt.getlayer(Dot11Elt).channel
 
-            #lock.acquire()
-            aps[bssid] = (bssid, ssid, channel)
-            #lock.release()
+            found_access_point(bssid, ssid, channel)
             
             #hidden networks
             if ssid == '' or pkt.getlayer(Dot11Elt).ID != 0:
@@ -51,7 +94,10 @@ def scan(iface):
 
     
 
-def start_scan(interface="wlan1mon"):
+def start_scan(t_remove: int, interface:str="wlan1mon"):
+    """
+    t_remove: if the access point has not been seen for this time, it is removed from the list of active access points
+    """
     #already running
     if _running.is_set():
         raise ValueError(f"Already started AP scan.")
@@ -64,6 +110,11 @@ def start_scan(interface="wlan1mon"):
 
     #scan packets to find beacon frames
     thread = Thread(target=scan, args=(interface, ), name="scanner")
+    thread.daemon = True
+    thread.start()
+
+    #remove old AccessPoints 
+    thread = Thread(target=clean, args=(t_remove, ), name="cleaner")
     thread.daemon = True
     thread.start()
 
